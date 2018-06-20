ba9cc6f1e14f7886359de616abdb3534b6fc1c37
Erik Crevel <erik.crevel@ntlworld.com>
2018-06-20 12:48:37 +0100

    Move usb code out of UsbService and into MainActivity.
    Link serial port parameters.

--- app/src/main/java/nimble/txbolttest/MainActivity.kt
+++ app/src/main/java/nimble/txbolttest/MainActivity.kt
@@ -1,18 +1,27 @@
 package nimble.txbolttest
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.ServiceConnection
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbDeviceConnection
+import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Message
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.Toast
+import com.felhr.usbserial.CDCSerialDevice
+import com.felhr.usbserial.UsbSerialDevice
+import com.felhr.usbserial.UsbSerialInterface
 
 import java.lang.ref.WeakReference
 
@@ -23,54 +32,143 @@ val TXBOLT_LAYOUT = KeyLayout(
 	mapOf()
 )
 
+val ACTION_USB_READY = "com.felhr.connectivityservices.USB_READY"
+val ACTION_USB_ATTACHED = "android.hardware.usb.action.USB_DEVICE_ATTACHED"
+val ACTION_USB_DETACHED = "android.hardware.usb.action.USB_DEVICE_DETACHED"
+val ACTION_USB_NOT_SUPPORTED = "com.felhr.usbservice.USB_NOT_SUPPORTED"
+val ACTION_NO_USB = "com.felhr.usbservice.NO_USB"
+val ACTION_USB_PERMISSION_GRANTED = "com.felhr.usbservice.USB_PERMISSION_GRANTED"
+val ACTION_USB_PERMISSION_NOT_GRANTED = "com.felhr.usbservice.USB_PERMISSION_NOT_GRANTED"
+val ACTION_USB_DISCONNECTED = "com.felhr.usbservice.USB_DISCONNECTED"
+val ACTION_CDC_DRIVER_NOT_WORKING = "com.felhr.connectivityservices.ACTION_CDC_DRIVER_NOT_WORKING"
+val ACTION_USB_DEVICE_NOT_WORKING = "com.felhr.connectivityservices.ACTION_USB_DEVICE_NOT_WORKING"
+val MESSAGE_FROM_SERIAL_PORT = 0
+private val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+private val BAUD_RATE = 9600 // BaudRate. Change this value if you need
+var SERVICE_CONNECTED = false
+
+private val DATA_BITS_MAP = mapOf(
+	Pair("5", UsbSerialInterface.DATA_BITS_5),
+	Pair("6", UsbSerialInterface.DATA_BITS_6),
+	Pair("7", UsbSerialInterface.DATA_BITS_7),
+	Pair("8", UsbSerialInterface.DATA_BITS_8)
+)
+
+private val STOP_BITS_MAP = mapOf(
+	Pair("1", UsbSerialInterface.STOP_BITS_1),
+	Pair("1.5", UsbSerialInterface.STOP_BITS_15),
+	Pair("2", UsbSerialInterface.STOP_BITS_2)
+)
+
+private val PARITY_MAP = mapOf(
+	Pair("None", UsbSerialInterface.PARITY_NONE),
+	Pair("Odd", UsbSerialInterface.PARITY_ODD),
+	Pair("Even", UsbSerialInterface.PARITY_EVEN),
+	Pair("Mark", UsbSerialInterface.PARITY_MARK),
+	Pair("Space", UsbSerialInterface.PARITY_SPACE)
+)
+
+private val FLOW_CONTROL_MAP = mapOf(
+	Pair("Off", UsbSerialInterface.FLOW_CONTROL_OFF),
+	Pair("RTS CTS", UsbSerialInterface.FLOW_CONTROL_RTS_CTS),
+	Pair("DSR DTR", UsbSerialInterface.FLOW_CONTROL_DSR_DTR),
+	Pair("XON XOFF", UsbSerialInterface.FLOW_CONTROL_XON_XOFF)
+)
+
 class MainActivity : AppCompatActivity()
 {
+	private var log: EditText? = null
+	private var strokes: EditText? = null
+
+	private val deviceList = mutableListOf<String>()
+	private var baudRate: EditText? = null
+	private var dataBits: Spinner? = null
+	private var stopBits: Spinner? = null
+	private var parity: Spinner? = null
+	private var flowControl: Spinner? = null
+
+	private var usbManager: UsbManager? = null
+	private var device: UsbDevice? = null
+	private var connection: UsbDeviceConnection? = null
+	private var serialPort: UsbSerialDevice? = null
+
+	private var serialPortConnected: Boolean = false
 	/*
-     * Notifications from UsbService will be received here.
+     * Different notifications from OS will be received here (USB attached, detached, permission responses...)
+     * About BroadcastReceiver: http://developer.android.com/reference/android/content/BroadcastReceiver.html
      */
-	private val mUsbReceiver = object : BroadcastReceiver()
+	private val usbReceiver = object : BroadcastReceiver()
 	{
 		override fun onReceive(context: Context, intent: Intent)
 		{
 			val message = when(intent.action)
 			{
-				UsbService.ACTION_USB_PERMISSION_GRANTED ->
+				ACTION_USB_PERMISSION_GRANTED ->
 					"USB Ready"
-				UsbService.ACTION_USB_PERMISSION_NOT_GRANTED ->
+				ACTION_USB_PERMISSION_NOT_GRANTED ->
 					"USB Permission not granted"
-				UsbService.ACTION_NO_USB ->
+				ACTION_NO_USB ->
 					"No USB connected"
-				UsbService.ACTION_USB_DISCONNECTED ->
+				ACTION_USB_DISCONNECTED ->
 					"USB disconnected"
-				UsbService.ACTION_USB_NOT_SUPPORTED ->
+				ACTION_USB_NOT_SUPPORTED ->
 					"USB device not supported"
 				else ->
 					null
 			}
-
 			if(message != null)
 			{
 				Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 				log?.append("$message\n")
 			}
+
+			when(intent.action)
+			{
+				ACTION_USB_PERMISSION ->
+				{
+					val granted = intent.extras!!.getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED)
+					if(granted)
+					// User accepted our USB connection. Try to open the device as a serial port
+					{
+						val intent = Intent(ACTION_USB_PERMISSION_GRANTED)
+						context.sendBroadcast(intent)
+						connection = usbManager!!.openDevice(device)
+						serialPortConnected = true
+						openDevice()
+					}
+					else
+					// User not accepted our USB connection. Send an Intent to the Main Activity
+					{
+						val intent = Intent(ACTION_USB_PERMISSION_NOT_GRANTED)
+						context.sendBroadcast(intent)
+					}
+				}
+				ACTION_USB_ATTACHED ->
+				{
+					refreshDevices()
+//					if(!serialPortConnected)
+//						findSerialPortDevice() // A USB device has been attached. Try to open it as a Serial port
+				}
+				ACTION_USB_DETACHED ->
+				{
+					refreshDevices()
+//					// Usb device was disconnected. send an intent to the Main Activity
+//					val intent = Intent(ACTION_USB_DISCONNECTED)
+//					context.sendBroadcast(intent)
+//					serialPortConnected = false
+//					serialPort!!.close()
+				}
+			}
 		}
 	}
-	private var usbService: UsbService? = null
-	private var log: EditText? = null
-	private var strokes: EditText? = null
-	private var handler: MyHandler? = null
-	private val usbConnection = object : ServiceConnection
-	{
-		override fun onServiceConnected(name: ComponentName, binder: IBinder)
-		{
-			usbService = (binder as UsbService.UsbBinder).service
-			usbService!!.handler = handler
-		}
 
-		override fun onServiceDisconnected(name: ComponentName)
-		{
-			usbService = null
-		}
+	/*
+     * Request user permission. The response will be received in the BroadcastReceiver
+     */
+	private fun requestUserPermission()
+	{
+		val mPendingIntent = PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+		usbManager!!.requestPermission(device, mPendingIntent)
 	}
 
 	override fun onCreate(savedInstanceState: Bundle?)
@@ -78,55 +176,52 @@ class MainActivity : AppCompatActivity()
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		handler = MyHandler(this)
+		this.usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+		findSerialPortDevice()
 
 		this.log = this.findViewById(R.id.log)
 		this.strokes = this.findViewById(R.id.strokes)
+
+		val devicesSpinner = this.findViewById<Spinner>(R.id.devices)
+		devicesSpinner.adapter = ArrayAdapter<String>(
+			this,
+			android.R.layout.simple_spinner_item,
+			this.deviceList)
+
+		this.baudRate = this.findViewById(R.id.baudrate)
+		this.dataBits = this.findViewById(R.id.data_bits)
+		this.stopBits = this.findViewById(R.id.stop_bits)
+		this.parity = this.findViewById(R.id.parity)
+		this.flowControl = this.findViewById(R.id.flow_control)
 	}
 
 	public override fun onResume()
 	{
 		super.onResume()
 		setFilters()  // Start listening notifications from UsbService
-		startService(UsbService::class.java, usbConnection, null) // Start UsbService(if it was not started before) and Bind it
+		this.log!!.append("onResume\n")
 	}
 
 	public override fun onPause()
 	{
 		super.onPause()
-		unregisterReceiver(mUsbReceiver)
-		unbindService(usbConnection)
-	}
-
-	private fun startService(service: Class<*>, serviceConnection: ServiceConnection, extras: Bundle?)
-	{
-		if(!UsbService.SERVICE_CONNECTED)
-		{
-			val startService = Intent(this, service)
-			if(extras != null && !extras.isEmpty)
-			{
-				val keys = extras.keySet()
-				for(key in keys)
-				{
-					val extra = extras.getString(key)
-					startService.putExtra(key, extra)
-				}
-			}
-			startService(startService)
-		}
-		val bindingIntent = Intent(this, service)
-		bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+		this.log!!.append("onPause\n")
+		unregisterReceiver(this.usbReceiver)
 	}
 
 	private fun setFilters()
 	{
 		val filter = IntentFilter()
-		filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED)
-		filter.addAction(UsbService.ACTION_NO_USB)
-		filter.addAction(UsbService.ACTION_USB_DISCONNECTED)
-		filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED)
-		filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED)
-		registerReceiver(mUsbReceiver, filter)
+		filter.addAction(ACTION_USB_PERMISSION)
+		filter.addAction(ACTION_USB_DETACHED)
+		filter.addAction(ACTION_USB_ATTACHED)
+
+		filter.addAction(ACTION_USB_PERMISSION_GRANTED)
+		filter.addAction(ACTION_NO_USB)
+		filter.addAction(ACTION_USB_DISCONNECTED)
+		filter.addAction(ACTION_USB_NOT_SUPPORTED)
+		filter.addAction(ACTION_USB_PERMISSION_NOT_GRANTED)
+		registerReceiver(this.usbReceiver, filter)
 	}
 
 	var currentStroke = Stroke(TXBOLT_LAYOUT, 0L)
@@ -148,6 +243,10 @@ class MainActivity : AppCompatActivity()
 				this.finishStroke()
 		}
 	}
+	private val mCallback = UsbSerialInterface.UsbReadCallback { data ->
+
+		this.receiveBytes(data)
+	}
 
 	fun finishStroke()
 	{
@@ -158,23 +257,105 @@ class MainActivity : AppCompatActivity()
 		}
 	}
 
-	/*
-     * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
-     */
-	private class MyHandler(activity: MainActivity) : Handler()
+	private fun refreshDevices()
 	{
-		private val activity = WeakReference(activity)
+		this.deviceList.clear()
+		this.deviceList.addAll(this.usbManager!!.deviceList.keys)
+	}
 
-		override fun handleMessage(msg: Message)
+	private fun findSerialPortDevice()
+	{
+		// This snippet will try to open the first encountered usb device connected, excluding usb root hubs
+		val usbDevices = usbManager!!.deviceList
+		if(!usbDevices.isEmpty())
 		{
-			when(msg.what)
+			var keep = true
+			for((_, value) in usbDevices)
 			{
-				UsbService.MESSAGE_FROM_SERIAL_PORT ->
+				device = value
+				val deviceVid = device!!.vendorId
+				val devicePid = device!!.productId
+
+				if(deviceVid != 0x1d6b && (devicePid != 0x0001 || devicePid != 0x0002 || devicePid != 0x0003))
 				{
-					val data = msg.obj as ByteArray
-					this.activity.get()!!.receiveBytes(data)
+					// There is a device connected to our Android device. Try to open it as a Serial Port.
+					requestUserPermission()
+					keep = false
+				}
+				else
+				{
+					connection = null
+					device = null
+				}
+
+				if(!keep)
+					break
+			}
+			if(!keep)
+			{
+				// There is no USB devices connected (but usb host were listed). Send an intent to MainActivity.
+				val intent = Intent(ACTION_NO_USB)
+				sendBroadcast(intent)
+			}
+		}
+		else
+		{
+			// There is no USB devices connected. Send an intent to MainActivity
+			val intent = Intent(ACTION_NO_USB)
+			sendBroadcast(intent)
+		}
+	}
+
+	private fun openDevice()
+	{
+		serialPort = UsbSerialDevice.createUsbSerialDevice(device!!, connection)
+		if(serialPort != null)
+		{
+			if(serialPort!!.open())
+			{
+				serialPort!!.setBaudRate(
+					this.baudRate?.text?.toString()?.toIntOrNull()
+						?: 9600)
+				serialPort!!.setDataBits(
+					DATA_BITS_MAP[this.dataBits?.selectedItem as? String]
+						?: UsbSerialInterface.DATA_BITS_8)
+				serialPort!!.setStopBits(
+					STOP_BITS_MAP[this.stopBits?.selectedItem as? String]
+						?: UsbSerialInterface.STOP_BITS_1)
+				serialPort!!.setParity(
+					PARITY_MAP[this.parity?.selectedItem as? String]
+						?: UsbSerialInterface.PARITY_NONE)
+				serialPort!!.setFlowControl(
+					FLOW_CONTROL_MAP[this.flowControl?.selectedItem as? String]
+						?: UsbSerialInterface.FLOW_CONTROL_OFF)
+
+				serialPort!!.read(mCallback)
+
+				// Everything went as expected. Send an intent to MainActivity
+				val intent = Intent(ACTION_USB_READY)
+				this.sendBroadcast(intent)
+			}
+			else
+			{
+				// Serial port could not be opened, maybe an I/O error or if CDC driver was chosen, it does not really fit
+				// Send an Intent to Main Activity
+				if(serialPort is CDCSerialDevice)
+				{
+					val intent = Intent(ACTION_CDC_DRIVER_NOT_WORKING)
+					this.sendBroadcast(intent)
+				}
+				else
+				{
+					val intent = Intent(ACTION_USB_DEVICE_NOT_WORKING)
+					this.sendBroadcast(intent)
 				}
 			}
+		}
+		else
+		{
+			// No driver for given device, even generic CDC driver could not be loaded
+			val intent = Intent(ACTION_USB_NOT_SUPPORTED)
+			this.sendBroadcast(intent)
 		}
 	}
 }
